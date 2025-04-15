d9f8729179f3615d0852355a12ac852aad4a1c2e
thanh29082005 <100671649+thanh29082005@users.noreply.github.com>
2025-04-15 23:50:30 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,23 +1,22 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# Đọc dữ liệu
+#read in the file
 movies_data = pd.read_csv("https://raw.githubusercontent.com/nv-thang/Data-Visualization-Course/main/Dataset%20for%20Practice/movies.csv")
-movies_data = movies_data.dropna()
 
-# Tính ngân sách trung bình theo thể loại
-avg_budget = movies_data.groupby('genre')['budget'].mean().round().reset_index()
+movies_data.dropna()
 
-# Dữ liệu
+st.write("""Average Movie Budget, Grouped by Genre""")
+avg_budget = movies_data.groupby('genre')['budget'].mean().round()
+avg_budget = avg_budget.reset_index()
 genre = avg_budget['genre']
 avg_bud = avg_budget['budget']
 
-# Vẽ biểu đồ PIE
-fig, ax = plt.subplots(figsize=(10, 10))
-ax.pie(avg_bud, labels=genre, autopct='%1.1f%%', startangle=140)
-ax.set_title("Tỷ lệ ngân sách trung bình theo thể loại phim")
+fig = plt.figure(figsize = (19, 10))
+plt.bar(genre, avg_bud, color = 'maroon')
+plt.xlabel('genre')
+plt.ylabel('budget')
+plt.title('Matplotlib Bar Chart Showing the Average \
+Budget of Movies in Each Genre')
 
-# Hiển thị lên Streamlit
-st.title("Biểu đồ Tỷ lệ Ngân sách theo Thể loại")
 st.pyplot(fig)
